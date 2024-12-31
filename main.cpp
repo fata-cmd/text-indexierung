52a23f4cc410e97ec8b7233eb3439a4f59810f47
fata <ulhig@student.kit.edu>
2024-12-31 22:11:23 +0100

    add malloc_count

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "util.h"
 #include <chrono>
+#include "malloc_count.h"
 #include <filesystem>
 
 template <typename SDict>
@@ -52,22 +53,26 @@ void benchmark(const std::string &inputFile, const std::string &queryFile, const
 {
     std::cerr << "Benchmark " << type_name<SDict>() << "\n";
     // construct
-    auto begin = std::chrono::steady_clock::now();
-    SDict trie = readFileIntoDict<SDict>(inputFile);
-    auto end = std::chrono::steady_clock::now();
-    auto c_dur = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+    std::chrono::_V2::steady_clock::time_point b, e;
+    int64_t c_dur, q_dur;
+    {
+        b = std::chrono::steady_clock::now();
+        SDict trie = readFileIntoDict<SDict>(inputFile);
+        e = std::chrono::steady_clock::now();
+        c_dur = std::chrono::duration_cast<std::chrono::milliseconds>(e - b).count();
 
-    // query
-    begin = std::chrono::steady_clock::now();
-    processQueries(trie, queryFile, resultFile);
-    end = std::chrono::steady_clock::now();
-    auto q_dur = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-    
+        // query
+        b = std::chrono::steady_clock::now();
+        processQueries(trie, queryFile, resultFile);
+        e = std::chrono::steady_clock::now();
+        q_dur = std::chrono::duration_cast<std::chrono::milliseconds>(e - b).count();
+    }
     // output
     std::cout << "RESULT name=fahri_taban trie_construction_time=" << c_dur
-              << " trie_construction_memory=" << 0
+              << " trie_construction_memory=" << malloc_count_peak()
               << " query_time=" << q_dur << "\n";
-
+    malloc_count_reset_peak();
+    
     std::cerr << "Query results written to " << resultFile << "\n\n";
 }
 
